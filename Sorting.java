8ccacb326915e95e5382dd8eee337d1ab11d0a88
Tarun <iamtarungupta21@gmail.com>
2025-01-25 19:17:12 +0530

    Contains sorting algorithms code

---
+++ Sorting.java
@@ -0,0 +1,62 @@
+import java.util.*;
+class Sorting {
+    public static void main(String[] a){
+
+        // Selection Sort Algorithm
+
+        // int[] arr = {13,46,24,52,20,9};
+
+        // for(int i=0; i<arr.length; i++){
+        //     int minI = i;
+        //     for(int j=i+1; j<arr.length; j++){
+        //         if(arr[j] < arr[minI]){
+        //             minI = j;
+        //         }
+        //     }
+        //     int temp = arr[minI];
+        //     arr[minI] = arr[i];
+        //     arr[i] = temp;
+        // }
+
+
+        // System.out.println(Arrays.toString(arr));
+
+
+        // Time Complexity = O(n^2)
+
+//----------------------------------------------------------------------------------------------------------------------------------------
+
+        // Bubble Sort Algorithm 
+
+        // int[] arr = {13,46,24,52,24,20,9};
+
+        // for(int i=0; i<arr.length-1; i++){
+        //     for(int j=0; j<arr.length-1; j++){
+        //         if(arr[j] > arr[j+1]){
+        //             int temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1] = temp;
+        //         }
+        //     }
+        // }
+
+        // System.out.println(Arrays.toString(arr));
+
+
+        // Time Complexity = O(n^2)
+
+-//-----------------------------------------------------------------------------------------------------------------------------------------------
+
+        
+
+
+
+
+
+
+        
+        
+
+
+    }
+}
